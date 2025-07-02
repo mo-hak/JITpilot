50b1aebcb747af5b6b1e3356b052061f2b3dec9d
MazyGio <mazy.eth@gmail.com>
2025-07-02 03:08:51 -0500

    run forge fmt

--- src/interfaces/IMaglevLens.sol
+++ src/interfaces/IMaglevLens.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.19;
 
 interface IMaglevLens {
-
     struct VaultGlobal {
         uint256 packed1; // cash, borrows, supply cap, borrow cap
         uint256 packed2; // shares, supply APY, borrow APY
     }
+
     function vaultsGlobal(address[] calldata vaults) external view returns (VaultGlobal[] memory output);
 }
